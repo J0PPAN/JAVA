087773bb644dbe1a9ec53942b1e6bfffd71fea83
Jovaan John Varghese <115357882+J0PPAN@users.noreply.github.com>
2022-11-29 22:26:19 +0530

    Using Multithreading To Print Odd and Even Numbers
    Please save the file containing the above code as "multithreading.java" in order for the java program to perfectly work.

---
+++ Multi Threading.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class multithreading implements Runnable{
+  public static void main(String args[]){
+    multithreading et = new multithreading();
+    Thread t1 = new Thread(et , "odd");
+    Thread t2 = new Thread(et , "even");
+    t1.start();
+    t2.start();
+  }
+synchronized public void run(){
+  Scanner sc = new Scanner(System.in);
+  String s1 = new String();
+  System.out.println("Enter the String with whitespace as the delimiter : ");
+  s1 = sc.nextLine();
+  int sum = 0 ;
+  StringTokenizer stk = new StringTokenizer(s1);
+  while(stk.hasMoreTokens()){
+    int n = Integer.parseInt(stk.nextToken());
+    if(Thread.currentThread().getName() == "even"){
+      if(n%2 == 0)
+        System.out.println(Thread.currentThread().getName()+n);
+     }
+    else if(Thread.currentThread().getName() == "odd"){
+      if(n%2 != 0)
+        System.out.println(Thread.currentThread().getName()+n);
+      }
+    }
+  }
+}
